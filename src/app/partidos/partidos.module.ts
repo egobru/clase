89bfcdf927b6384dc3d41aba5298d0cb635b667e
Quique <45952571+egobru@users.noreply.github.com>
2019-07-01 10:45:39 +0200

    1julio

--- src/app/partidos/partidos.module.ts
+++ src/app/partidos/partidos.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule} from '@angular/forms';
 import { PartidosRoutingModule } from './partidos-routing.module';
 import { PartidosComponent } from './partidos/partidos.component';
 import { PartidoFormComponent } from './partido-form/partido-form.component';
@@ -10,7 +10,8 @@ import { ResumenComponent } from './resumen/resumen.component';
   declarations: [PartidosComponent, PartidoFormComponent, ResumenComponent],
   imports: [
     CommonModule,
-    PartidosRoutingModule
+    PartidosRoutingModule,
+    FormsModule
   ]
 })
 export class PartidosModule { }

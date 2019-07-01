b4931512aa1512b35549af86ae2450593584a668
Quique <45952571+egobru@users.noreply.github.com>
2019-07-01 10:44:33 +0200

    1julio

--- src/app/partidos/partido-form/partido-form.component.ts
+++ src/app/partidos/partido-form/partido-form.component.ts
@@ -8,14 +8,43 @@ import { environment } from 'src/environments/environment';
   styles: []
 })
 export class PartidoFormComponent implements OnInit {
-partido;
+  partido: Partido;
+  alertClick = () => this.partido.local += '*';
 
   constructor(activateRoute: ActivatedRoute) {
-    let id = activateRoute.snapshot.params.id; // ['id'];
-    this.partido = environment.partidos.find( p => p.id == id);
-  }
+    const id = activateRoute.snapshot.params.id;
+// tslint:disable-next-line: triple-equals
+    const p = environment.partidos.find(x => x.id == id);
+    const sucesos: Suceso[] = p.sucesos;
+    const goles: Suceso[] = sucesos.filter(s => s.class === 'Gol');
+
+    this.partido = {
+      local: p.idLocal,
+      visitante: p.idVisitante,
+      golLocal: goles.filter(g => g.idParticipante === p.idLocal).length,
+      golVisitante: goles.filter(g => g.idParticipante === p.idVisitante).length,
+      fecha: p.timeStamp
+    };
+
+    // Podría hacerme los partidos dinámicamente cogiendo sólo el id
+    // this.partido = {id: id, local:'A', visitante:'b'};
+   }
 
   ngOnInit() {
   }
 
+}
+
+export interface Partido {
+  local: string;
+  visitante: string;
+  golLocal: number;
+  golVisitante: number;
+  fecha: number;
+  // getResultado: () => string;
+}
+
+export interface Suceso {
+  class: string;
+  idParticipante: string;
 }
